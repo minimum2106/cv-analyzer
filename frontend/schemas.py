d4c8c95715bd530a740e3eb901584857d30eca98
minimum2106 <tranhongquan2106@gmail.com>
2025-07-25 12:53:43 +0200

    add new schema for CV and Job line

---
+++ frontend/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Tuple, List, Optional
+
+class JobReqInfo(BaseModel):
+    position: Tuple[float, float, float, float]
+    text: str
+    explanation: str
+    
+
+class CVLineInfo(BaseModel):
+    position: Tuple[float, float, float, float]
+    text: str
+    connected_job_reqs: Optional[List[JobReqInfo]] = None
+
